e48073bd003b03d863e05d6a9cfd10a9a7c4307d
Josh Benner <josh@bennerweb.com>
2017-09-23 14:32:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+RabbitMQ
+========
+
+Install and configure RabbitMQ, including clustering.
+
+See defaults file for configuration options.
+
+License
+-------
+
+BSD
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,39 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_rabbit_service(host):
+    service = host.service('rabbitmq-server')
+    assert service.is_running
+    assert service.is_enabled
+
+
+def test_rabbit_listening(host):
+    assert host.socket('tcp://0.0.0.0:5672').is_listening
+    assert host.socket('tcp://0.0.0.0:5671').is_listening
+
+
+def test_rabit_management_listening(host):
+    assert host.socket('tcp://0.0.0.0:15672').is_listening
+
+
+def test_config_file(host):
+    f = host.file('/etc/rabbitmq/rabbitmq.config')
+    assert f.is_file
+    assert f.user == 'rabbitmq'
+    assert f.group == 'rabbitmq'
+    assert f.mode == 0o644
+    assert f.contains('connection, info')
+
+
+def test_env_file(host):
+    f = host.file('/etc/rabbitmq/rabbitmq-env.conf')
+    assert f.is_file
+    assert f.user == 'rabbitmq'
+    assert f.group == 'rabbitmq'
+    assert f.mode == 0o644
+    assert f.contains('RABBITMQ_NODENAME="rabbit"')
